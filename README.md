a0cd617e4241b00ac98b2a91401f2f3cccd3ada0
stefanlasiewski <stefan@stefanco.com>
2017-11-16 13:34:41 -0800

    First checkin.

---
+++ README.md
@@ -0,0 +1,25 @@
+# vagrant-centos-docker
+
+Provides a CentOS 7 host running Docker CE, installed per https://docs.docker.com/engine/installation/linux/docker-ce/centos/
+
+Installs CentOS, Docker CE and tests the finished image with `docker run hello-world`.
+
+Supports Docker CE Stable, Edge and Test. See `install-docker.sh` for details.
+
+# Usage
+
+Simply clone this repo, and run `vagrant up`:
+
+```
+$ vagrant up
+Bringing machine 'default' up with 'virtualbox' provider...
+==> default: Importing base box 'centos/7'...
+...
+==> default: + sudo docker run hello-world
+...
+==> default: Status: Downloaded newer image for hello-world:latest
+==> default: 
+==> default: Hello from Docker!
+==> default: This message shows that your installation appears to be working correctly.
+...
+```
---
+++ install-docker.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Install Docker on CentOS 7 per
+# https://docs.docker.com/engine/installation/linux/docker-ce/centos/
+
+set -e # Exit if any subcommand fails
+set -x # Print commands for troubleshooting
+
+# 1. Install required packages.
+
+sudo yum install --assumeyes --quiet yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+# 2. Use the following command to set up the stable repository. You always need the stable repository, even if you want to install builds from the edge or test repositories as well.
+
+sudo yum-config-manager --quiet \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+# 3. Optional: Enable the edge and test repositories.
+
+# yum-config-manager --enable docker-ce-edge
+# yum-config-manager --enable docker-ce-test
+
+# INSTALL DOCKER CE
+
+# 1. Install the latest version of Docker CE
+
+sudo yum install --assumeyes --quiet docker-ce
+
+# 3. Start Docker.
+
+sudo systemctl start docker
+
+# 4. Verify that docker is installed correctly by running the hello-world image.
+
+sudo docker run hello-world
