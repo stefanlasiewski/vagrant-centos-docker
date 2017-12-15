8aebf1b49f752801c1f58561af85301efe71ef89
stefanlasiewski <stefan@stefanco.com>
2017-12-14 17:05:07 -0800

    Tell user what we are doing.

--- enable-user-namespaces.sh
+++ enable-user-namespaces.sh
@@ -5,26 +5,29 @@
 # * https://gist.github.com/dpneumo/279d6bc5dcbe5609cfcb8ec48499701a
 # * https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux_atomic_host/7/html-single/getting_started_with_containers/index
 
+set -e # Exit if any subcommand fails
+#set -x # Print commands for troubleshooting
+
 enable-user-namespaces () {
 
-  # Add the namespace.unpriv_enable=1 option to the kernel (vmlinuz*) command line.
+  echo "Add the namespace.unpriv_enable=1 option to the kernel (vmlinuz*) command line."
   grubby --args="user_namespace.enable=1" --update-kernel="$(grubby --default-kernel)"
 
-  # Add a value to the user.max_user_namespaces kernel tuneable so it is set permanently as follows:
+  echo "Add a value to the user.max_user_namespaces kernel tuneable so it is set permanently"
   echo "user.max_user_namespaces=15076" >> /etc/sysctl.conf
 
-  # Assign users and groups to be mapped by user namespaces. 
+  echo "Assign users and groups to be mapped by user namespaces."
   echo dockremap:808080:1000 >> /etc/subuid
   echo dockremap:808080:1000 >> /etc/subgid
 
-  # Copy daemon.json which enables User Namespaces
+  echo "Copy daemon.json which enables User Namespaces"
   # TODO: What if daemon.json already exists?
   cp -v /vagrant/daemon.json /etc/docker/daemon.json
 }
 
 enable-user-namespaces
 
-echo "Reboot the system"
+echo "User Namespaces now enabled. Reboot the system to activate them."
 
 echo "After reboot, check that User Namespaces are enabled per https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux_atomic_host/7/html-single/getting_started_with_containers/index." 
 
